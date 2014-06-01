d6211ed66739dc88bc8cacdcfddb1108908fa200
Cristian Cienfuegos <cristian100@gmail.com>
2014-05-31 19:34:03 -0600

    added stuff for login

--- modules/cambox_chat/themes/cambox_chat-comments.tpl.php
+++ modules/cambox_chat/themes/cambox_chat-comments.tpl.php
@@ -4,6 +4,15 @@
     <?php print $form;?>
   </div>
 <?php endif;?>
+<?php if (isset($login_form) && !empty($login_form) ):?>
+  <div id="login-form">
+    <h2>Chat en Vivo</h2>
+    <div class="content">
+      <p>Para entrar al Chat inicia sesión</p>
+      <?php print $login_form;?>
+    </div>
+  </div>
+<?php endif;?>
   <div class="comments">
     <?php print $comments;?>
   </div>
